2ff59d3f51cfdc47acd13aced2d82fe8ebacbc04
Jorge Roldán <icabina@live.com>
2022-02-05 09:47:47 -0500

    m

---
+++ SumChilds.js
@@ -0,0 +1,22 @@
+const aVeryBigSum = (n, arr) => {
+  let array = arr.split(" ");
+
+  const toNums = (arr) => {
+    let newArr = [];
+    for (let a of arr) {
+      newArr.push(Number(a));
+    }
+    return newArr;
+  };
+  let arrayNums = toNums(array);
+
+  const acumular = (total, item) => total + item;
+  let arrayNumsRed = arrayNums.reduce(acumular, 0);
+
+  return arrayNumsRed;
+};
+// console.time();
+console.log(
+  aVeryBigSum("5", "1000000001 1000000002 1000000003 1000000004 1000000005")
+);
+// console.timeEnd();
