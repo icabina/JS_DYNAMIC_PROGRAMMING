31498fecd08dede34b59c1e4563b4266d4ab8866
Jorge Roldán <icabina@live.com>
2022-02-11 16:36:18 -0500

    m

--- js/heapSort.js
+++ js/heapSort.js
@@ -5,7 +5,7 @@
 
 // JavaScript program for implementation
 // of Heap Sort
-
+//====================================================
 function sort(arr) {
   var n = arr.length;
 
@@ -13,9 +13,12 @@ function sort(arr) {
   // Math.round(5.5) // Answer 6, rounds to the closest whole number.
   // Math.floor(5.5) // Answer 5, rounds down.
 
+  //[5, 12, 11, 13, 4, 6, 7];
   // Build heap (rearrange array)
+
   for (var i = Math.floor(n / 2) - 1; i >= 0; i--) heapify(arr, n, i);
 
+  console.log(arr); // [13, 12, 11, 5, 4, 6, 7]
   // One by one extract an element from heap
   for (var i = n - 1; i > 0; i--) {
     // Move current root to end
@@ -26,21 +29,26 @@ function sort(arr) {
     // call max heapify on the reduced heap
     heapify(arr, i, 0);
   }
-}
 
+  console.log(arr); // [4, 5, 6, 7, 11, 12, 13]
+}
+//====================================================
 // To heapify a subtree rooted with node i which is
 // an index in arr[]. n is size of heap
 function heapify(arr, n, i) {
+  // console.log(arr);
   var largest = i; // Initialize largest as root
   var l = 2 * i + 1; // left = 2*i + 1
   var r = 2 * i + 2; // right = 2*i + 2
 
+  // [ 12, 5, 14, 13, 4, 6, 7];
   // If left child is larger than root
   if (l < n && arr[l] > arr[largest]) largest = l;
 
   // If right child is larger than largest so far
   if (r < n && arr[r] > arr[largest]) largest = r;
 
+  // console.log(arr);
   // If largest is not root
   if (largest != i) {
     var swap = arr[i];
@@ -50,14 +58,17 @@ function heapify(arr, n, i) {
     // Recursively heapify the affected sub-tree
     heapify(arr, n, largest);
   }
+  console.log(arr);
 }
+// heapify([5, 12, 11, 13, 4, 6, 7], 7, 0);
 
+//====================================================
 /* A utility function to print array of size n */
 function printArray(arr) {
   var n = arr.length;
   for (var i = 0; i < n; ++i) document.write(arr[i] + " ");
 }
-
+//====================================================
 var arr = [5, 12, 11, 13, 4, 6, 7];
 var n = arr.length;
 
@@ -65,6 +76,8 @@ sort(arr);
 
 document.write("Sorted array is <br>");
 printArray(arr, n);
+// Sorted array is
+// 5 6 7 11 12 13
 
 // This code is contributed by SoumikMondal
 
